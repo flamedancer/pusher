ad55ecf27540ddb5359f771281584a251cfef423
guochen <guochen070613@gmail.com>
2014-09-15 18:08:10 +0800

    a rect for the model of a pusher

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ function World(gs) {
 
     this.draw = function() {
         gs.clear();
-        gs.background('rgba(200, 200, 200, 1.0)');
+        gs.background('rgba(255, 255, 255, 1.0)');
     }
 }
 
 function launch() {
     // use the DIV tag with Id of 'surface' as our game surface
-    var gs = new JSGameSoup("surface", 30);
+    var gs = new JSGameSoup(document.getElementById("surface"), 30);
     // add an instance of the 'Dot' entity class above to our game
     gs.addEntity(new World(gs));
     gs.addEntity(new Pusher(gs));
@@ -20,13 +20,17 @@ function Pusher(gs) {
     this.x = 40;
     this.y = 40;
     this.speed = 10;
-    this.poly = [ [0, 0], [80, 80], [80, 0] ]; 
+
+    this.update = function() {
+    }
     
 
-
     this.draw = function(c) {
-        c.strokeStyle = 'rgba(255, 255, 255, 1.0)';
-        gs.polygon(this.poly);
+        c.fillStyle = 'rgba(200, 200, 200, 0.5)';
+        c.beginPath();
+        c.rect(0, 0, 60, 60);
+        c.closePath();
+        c.fill();
     }
 
     

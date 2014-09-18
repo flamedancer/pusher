fede8bcd2393713f99d37aa1a177c447a9f41f6b
guochen <guochen070613@gmail.com>
2014-09-18 18:11:43 +0800

    add some enemy which can be hit by boxes

--- fore/js/main.js
+++ fore/js/main.js
@@ -2,11 +2,19 @@ function main(gs) {
     /*** Define some different types of things ***/
     t_pusher = 1;
     t_box = 2;
+    t_ai = 3;
 
     WIDTH = gs.width;
     HEIGHT = gs.height;
 
     pic = 30;
+
+    boxes = [];  
+    Ai_pushers = [];  
+
+    box_num = 20;
+    Ai_num = 8;
+
 
     function Pusher(gs) {
         this.type = t_pusher;
@@ -38,8 +46,6 @@ function main(gs) {
             this.direction = [0, 0];
         }
 
-
-
         this.update = function() {
             this.x += this.direction[0] * this.speed;
             this.y += this.direction[1] * this.speed;
@@ -54,7 +60,6 @@ function main(gs) {
                 this.y = this.y - this.speed;
         }
         
-
         this.draw = function(c) {
             c.fillStyle = 'rgba(200, 200, 200, 0.5)';
             c.beginPath();
@@ -64,7 +69,6 @@ function main(gs) {
         }
 
         this.get_collision_aabb = function() {
-        
             return [this.x, this.y, pic, pic];
         }
 
@@ -78,10 +82,77 @@ function main(gs) {
     }
 
 
-    function Box(gs) {
+    function Ai_pusher(x, y) {
+        this.type = t_ai;
+        
+        this.x = x * pic;
+        this.y = y * pic;
+        this.speed = 4;
+
+        var all_directions = [[0, -1], [0, 1], [-1, 0], [1, 0]];
+        this.direction = all_directions[Math.floor(Math.random() * 4)]
+
+        this.update = function() {
+            this.x += this.direction[0] * this.speed;
+            this.y += this.direction[1] * this.speed;
+            if (this.x < 0)
+                this.x = WIDTH;
+            else if (this.x > WIDTH)
+                this.x = 0;
+            else if (this.y < 0)
+                this.y = HEIGHT;
+            else if (this.y > HEIGHT)
+                this.y = 0;
+
+            // 1/5的概率 自动换方向
+            if (Math.random() <= 0.05) {
+                var all_directions = [[0, -1], [0, 1], [-1, 0], [1, 0]];
+                this.direction = all_directions[Math.floor(Math.random() * 4)]
+            }
+        }
+        
+
+        this.draw = function(c) {
+            c.fillStyle = 'rgba(50, 40, 20, 1.0)';
+            c.beginPath();
+            c.rect(this.x, this.y, pic, pic);
+            c.closePath();
+            c.fill();
+        }
+ 
+        this.get_collision_aabb = function() {
+            return [this.x, this.y, pic, pic];
+        }
+
+        this.collide_aabb = function(who) {
+            if (who.type == t_box) {
+                if (who.direction[0] == 0 && who.direction[1] == 0) {
+                    this.direction = [ -this.direction[0], -this.direction[1]]
+                    this.x += this.direction[0] * this.speed;
+                    this.y += this.direction[1] * this.speed;
+                }
+                else {
+                    this.destroy();
+                    who.destroy();
+                }
+            }
+        }
+
+        this.destroy = function(c) {
+            this.x = -100;
+            this.y = -100;
+            gs.delEntity(this);
+        }
+        
+       
+
+    }
+
+
+    function Box(x, y) {
         this.type = t_box;
-        this.x = 300;
-        this.y = 300;
+        this.x = x * pic;
+        this.y = y * pic;
         this.direction = [0, 0];
         this.speed = 2;
 
@@ -119,6 +190,11 @@ function main(gs) {
             }
         }
 
+        this.destroy = function(c) {
+            this.x = -100;
+            this.y = -100;
+            gs.delEntity(this);
+        }
     }
 
 
@@ -126,11 +202,18 @@ function main(gs) {
 
 
         var pusher = new Pusher(gs);
-        var boxes = [];  
 
         this.init = function(gs) {
             gs.addEntity(pusher);
-            boxes.push(gs.addEntity(new Box(gs)));
+            for (var n=0; n<box_num; n++) {
+                var locat = Math.floor(Math.random() * 200) 
+                boxes.push(gs.addEntity(new Box(locat / 10, locat % 10)));
+            }
+
+            for (var n=0; n<Ai_num; n++) {
+                var locat = Math.floor(Math.random() * 200) 
+                Ai_pushers.push(gs.addEntity(new Ai_pusher(locat / 10, locat % 10)));
+            }
         }
 
         this.draw = function() {
@@ -141,6 +224,7 @@ function main(gs) {
         this.update = function() {
 
             collide.aabb([pusher], boxes);
+            collide.aabb(Ai_pushers, boxes);
         }
     }
 
